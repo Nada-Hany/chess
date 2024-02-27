b80cecf7994690d34477e9996bd2f2181d9231c6
Nada-Hany <nadaaayman993@gmail.com>
2024-02-27 20:50:17 +0200

    castle bug fixed

--- Piece.py
+++ Piece.py
@@ -9,8 +9,19 @@ class piece:
         self.previousX = -1
         self.previousY = -1    
         self.moved = False
-        self.canCastle = False
         self.possibleMoves = []
+        self.canCastle = False
+
+        self.leftRookCastleX = 3
+        self.rightRookCastleX = 5
+        self.kingCastleRightX = 6
+        self.kingCastleLeftX = 2
+        self.initialX = -1
+        self.initialY = -1
+
+    def SetInitialCor(self, x, y):
+        self.initialX = x
+        self.initialY = y
 
     def Moved(self):
         self.moved = True
